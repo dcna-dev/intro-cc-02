29609b20f177db2be4c5b9425fcbe6fc9b94c21a
Diego Cananea <diegocananea@gmail.com>
2018-10-13 11:41:45 -0300

    Desafio de multiplicacao de matrizes

--- lista_exercicios_02/desafio_multiplicacao_matrizes.py
+++ lista_exercicios_02/desafio_multiplicacao_matrizes.py
@@ -21,19 +21,23 @@ def cria_matriz(num_linhas, num_col, valor = 0):
 
 def multiplica_matrizes(m1, m2):
 
-    num_linhas = len(m1)
+    num_linhas_m1, num_colunas_m1 = len(m1), len(m1[0])
     num_col = len(m2[0])
 
-    matriz_result = cria_matriz(num_linhas, num_col)
-
-    for linha in range(num_linhas):
+    matriz_result = cria_matriz(num_linhas_m1, num_col)
+    for linha in range(num_linhas_m1):
         for coluna in range(num_col):
-            matriz_result[linha][coluna] = m1[linha][coluna] * m2[linha][coluna] + m1[linha+1][coluna+1] * m2[linha+1][coluna+1]
+            for k in range(num_colunas_m1):
+                matriz_result[linha][coluna] += m1[linha][k]*m2[k][coluna]
     return matriz_result
 
 def test_multiplica_matrizes():
     m1 = [[1,2],[3,4]]
     m2 = [[-1, 3], [4, 2]]
     assert multiplica_matrizes(m1, m2) == [[7,7], [13,17]]
+
+    m1 = [[2, 3],[0, 1], [-1,4]]
+    m2 = [1, 2, 3], [-2, 0, 4]
+    assert multiplica_matrizes(m1, m2) == [[-4, 4, 18], [-2, 0, 4], [-9, -2, 13]]
 
 
